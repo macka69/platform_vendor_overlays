27bcddbe880e89c756b6e914770291aab727e398
rohan <purohit.rohan@gmail.com>
2020-04-20 06:01:36 -0400

    themes: move some overlays from vendor/bliss [1/2]
    Change-Id: I2892ba1aa8f0593edb135cb7756ac26277d5f13b

--- bliss_themes.mk
+++ bliss_themes.mk
@@ -29,6 +29,12 @@
 #PRODUCT_PACKAGES += \
 #    BlissFonts
 
+ # Overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/themes/prebuilt/system/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
 
